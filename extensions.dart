ab6a9347f23c2c945b85d1e8d419438ba1aeba46
mohamed hussin <65326535+mhussin2008@users.noreply.github.com>
2024-01-06 16:14:35 +0200

    updated

--- extensions.dart
+++ extensions.dart
@@ -1,145 +1,62 @@
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
-
 extension BuildContextEntension<T> on BuildContext {
+  bool get isMobile => this.width <= 500.0;
 
-  bool get isMobile =>
-      MediaQuery
-          .of(this)
-          .size
-          .width <= 500.0;
+  bool get isTablet => this.width < 1024.0 && this.width >= 650.0;
 
-  bool get isTablet =>
-      MediaQuery
-          .of(this)
-          .size
-          .width < 1024.0 && MediaQuery
-          .of(this)
-          .size
-          .width >= 650.0;
+  bool get isSmallTablet => this.width < 650.0 && this.width > 500.0;
 
-  bool get isSmallTablet =>
-      MediaQuery
-          .of(this)
-          .size
-          .width < 650.0 && MediaQuery
-          .of(this)
-          .size
-          .width > 500.0;
+  bool get isDesktop => this.width >= 1024.0;
+  // MediaQuery
+  //     .of(this)
+  //     .size
+  //     .width >= 1024.0;
 
-  bool get isDesktop =>
-      MediaQuery
-          .of(this)
-          .size
-          .width >= 1024.0;
+  bool get isSmall => this.width < 850.0 && this.width >= 560.0;
 
-  bool get isSmall =>
-      MediaQuery
-          .of(this)
-          .size
-          .width < 850.0 && MediaQuery
-          .of(this)
-          .size
-          .width >= 560.0;
+  double get width => MediaQuery.of(this).size.width;
 
-  double get width =>
-      MediaQuery
-          .of(this)
-          .size
-          .width;
+  double get height => MediaQuery.of(this).size.height;
 
-  double get height =>
-      MediaQuery
-          .of(this)
-          .size
-          .height;
-
-  Size get size =>
-      MediaQuery
-          .of(this)
-          .size;
-
+  Size get size => MediaQuery.of(this).size;
 
   // text styles
 
-  TextStyle? get displayMedium =>
-      Theme
-          .of(this)
-          .textTheme
-          .displayMedium;
+  TextStyle? get displayMedium => Theme.of(this).textTheme.displayMedium;
 
-  TextStyle? get displaySmall =>
-      Theme
-          .of(this)
-          .textTheme
-          .displaySmall;
+  TextStyle? get displaySmall => Theme.of(this).textTheme.displaySmall;
 
-  TextStyle? get headlineLarge =>
-      Theme
-          .of(this)
-          .textTheme
-          .headlineLarge;
+  TextStyle? get headlineLarge => Theme.of(this).textTheme.headlineLarge;
 
-  TextStyle? get headlineMedium =>
-      Theme
-          .of(this)
-          .textTheme
-          .headlineMedium;
+  TextStyle? get headlineMedium => Theme.of(this).textTheme.headlineMedium;
 
-  TextStyle? get titleLarge =>
-      Theme
-          .of(this)
-          .textTheme
-          .titleLarge;
+  TextStyle? get titleLarge => Theme.of(this).textTheme.titleLarge;
 
-  TextStyle? get titleMedium =>
-      Theme
-          .of(this)
-          .textTheme
-          .titleMedium;
+  TextStyle? get titleMedium => Theme.of(this).textTheme.titleMedium;
 
-  TextStyle? get titleSmall =>
-      Theme
-          .of(this)
-          .textTheme
-          .titleSmall;
+  TextStyle? get titleSmall => Theme.of(this).textTheme.titleSmall;
 
-  TextStyle? get labelLarge =>
-      Theme
-          .of(this)
-          .textTheme
-          .labelLarge;
+  TextStyle? get labelLarge => Theme.of(this).textTheme.labelLarge;
 
-  TextStyle? get bodySmall =>
-      Theme
-          .of(this)
-          .textTheme
-          .bodySmall;
+  TextStyle? get bodySmall => Theme.of(this).textTheme.bodySmall;
 
-  TextStyle? get titleTextStyle =>
-      Theme
-          .of(this)
-          .appBarTheme
-          .titleTextStyle;
+  TextStyle? get titleTextStyle => Theme.of(this).appBarTheme.titleTextStyle;
 
   TextStyle? get bodyExtraSmall =>
       bodySmall?.copyWith(fontSize: 10, height: 1.6, letterSpacing: .5);
 
-  TextStyle? get bodyLarge =>
-      Theme
-          .of(this)
-          .textTheme
-          .bodyLarge;
+  TextStyle? get bodyLarge => Theme.of(this).textTheme.bodyLarge;
 
   TextStyle? get dividerTextSmall => bodySmall?.copyWith(
       letterSpacing: 0.5, fontWeight: FontWeight.w700, fontSize: 12.0);
 
-  TextStyle? get dividerTextLarge => bodySmall?.copyWith(letterSpacing: 1.5,
+  TextStyle? get dividerTextLarge => bodySmall?.copyWith(
+      letterSpacing: 1.5,
       fontWeight: FontWeight.w700,
       fontSize: 13.0,
       height: 1.23);
-
 
   // colors
   Color get primaryColor => Theme.of(this).primaryColor;
@@ -163,11 +80,11 @@ extension BuildContextEntension<T> on BuildContext {
   Color get background => Theme.of(this).colorScheme.background;
 
   Future<T?> showBottomSheet(
-      Widget child, {
-        bool isScrollControlled = true,
-        Color? backgroundColor,
-        Color? barrierColor,
-      }) {
+    Widget child, {
+    bool isScrollControlled = true,
+    Color? backgroundColor,
+    Color? barrierColor,
+  }) {
     return showModalBottomSheet(
       context: this,
       barrierColor: barrierColor,
@@ -177,7 +94,8 @@ extension BuildContextEntension<T> on BuildContext {
     );
   }
 
-  ScaffoldFeatureController<SnackBar, SnackBarClosedReason> showSnackBar(String message) {
+  ScaffoldFeatureController<SnackBar, SnackBarClosedReason> showSnackBar(
+      String message) {
     return ScaffoldMessenger.of(this).showSnackBar(
       SnackBar(
         content: Text(message),
@@ -197,6 +115,4 @@ extension BuildContextEntension<T> on BuildContext {
       textColor: onPrimary,
     );
   }
-
-
 }
